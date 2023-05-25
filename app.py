1cac2abc790c738f648b2bdd0e095fe918c3a63b
Raphael Torres <74460413+raphaeltcf@users.noreply.github.com>
2023-05-25 19:30:49 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import json
+from flask import Flask, request, jsonify
+import pickle
+import tensorflow as tf
+from keras.models import load_model
+from extract import class_prediction, get_response
+
+
+model = load_model('model.h5')
+intents = json.loads(open('intents.json', encoding='utf-8').read())
+
+
+app = Flask(__name__)
+
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    data = request.get_json()
+    message = data['message']
+
+    ints = class_prediction(message, model)
+    res = get_response(ints, intents)
+
+
+    return jsonify({'response': res})
+
+if __name__ == '__main__':
+    app.run(debug=True)
