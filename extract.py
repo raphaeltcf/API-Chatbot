545b0b2cbeb648ea044678670fba00bba5d8de6e
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2023-05-27 17:05:05 -0300

    [FIX] Bugs

--- extract.py
+++ extract.py
@@ -8,40 +8,32 @@ lemmatizer = WordNetLemmatizer()
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 
-
 def clear_writing(writing):
     sentence_words = nltk.word_tokenize(writing)
     return [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
 
 def bag_of_words(writing, words):
     sentence_words = clear_writing(writing)
-    bag = [0]*len(words)
-    for setence in sentence_words:
+    bag = [0] * len(words)
+    for sentence in sentence_words:
         for i, word in enumerate(words):
-            if word == setence:
+            if word == sentence:
                 bag[i] = 1
-    return(np.array(bag))
+    return np.array(bag)
 
 def class_prediction(writing, model):
     prevision = bag_of_words(writing, words)
     response_prediction = model.predict(np.array([prevision]))[0]
     results = [[index, response] for index, response in enumerate(response_prediction) if response > 0.25]
-    if "1" not in str(prevision) or len(results) == 0 : 
+    if "1" not in str(prevision) or len(results) == 0:
         results = [[0, response_prediction[0]]]
     results.sort(key=lambda x: x[1], reverse=True)
-    return [{"intent": classes[r[0]], "probality": str(r[1])} for r in results]
+    return [{"intent": classes[r[0]], "probability": str(r[1])} for r in results]
 
 def get_response(intents, intents_json):
     tag = intents[0]['intent']
     list_of_intents = intents_json['intents']
-    result = "Desculpe, não tenho uma resposta para isso no momento."
     for idx in list_of_intents:
         if idx['tag'] == tag:
-            result = random.choice(idx['responses'])
-            break
-    if result == "Desculpe, não tenho uma resposta para isso no momento.":
-        return result
-    else:
-        return {"response": result}
-
-
+            return random.choice(idx['responses'])
+    return random.choice(intents_json['fallback'])
